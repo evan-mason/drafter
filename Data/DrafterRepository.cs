7616503345398aaeeb1c96a17e4607df11bd46e9
Evan Mason <evan.mason@wexinc.com>
2023-10-19 15:40:28 +1100

    Hidden picks until 10 seconds after they were selected
    Hidden picks until 10 seconds after they were selected

--- Data/DrafterRepository.cs
+++ Data/DrafterRepository.cs
@@ -332,7 +332,7 @@ namespace Drafter.Data
             return _ctx.Players
                 .Include(p => p.FantasyTeam)
                 .OrderBy(p => p.DraftPosition)
-                .Where(p => p.FantasyTeam.DrafterUser != FreeAgentUser) // shouldn't need calling but eh.
+                .Where(p => p.FantasyTeam.DrafterUser != FreeAgentUser && p.DraftTime < DateTime.Now.AddSeconds(-10)) // shouldn't need calling but eh.)
                 .Select(p => new PlayerDto() { Id = p.DraftPosition, Name = p.Name, Position = p.Position, FantasyPoints = p.FantasyPointsAverage, FantasyTeam = p.FantasyTeam.Name, NBATeam = p.NBATeam, PlayerPictureId = p.PlayerPictureId })
                 .ToList();
         }
